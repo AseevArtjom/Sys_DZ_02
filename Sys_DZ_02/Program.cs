a484fd22de62ba205defd44ff2abf167e3c5d088
AseevArtjom <Artjom4ik2008@gmail.com>
2024-03-28 15:43:27 +0300

    Add project files.

---
+++ Sys_DZ_02/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sys_DZ_02
+{
+    //Написать класс Worker и несколько классов-наследников (напр, Manager, Director, Programmer),
+    //определить в них методы и атрибуты. Данные классы скомпилировать в отдельный dll-файл.
+    //Далее написать программу, в которой данный dll загружается и создать несколько работников и вызвать их методы.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AssemblyName assemblyName = AssemblyName.GetAssemblyName(@"Workers.dll");
+            Assembly assembly = Assembly.Load(assemblyName);
+
+            Module module = assembly.GetModule("Workers.dll");
+
+            Type Worker = module.GetType("Workers.Worker");
+            Type Manager = module.GetType("Workers.Manager");
+            Type Programmer = module.GetType("Workers.Programmer");
+
+            ConstructorInfo workerConstructor = Worker.GetConstructor(new Type[] { typeof(string) });
+            ConstructorInfo managerConstructor = Manager.GetConstructor(new Type[] { typeof(string) });
+            ConstructorInfo programmerConstructor = Programmer.GetConstructor(new Type[] { typeof(string) });
+
+            dynamic w = workerConstructor.Invoke(new object[] { "Ivan" });
+            w.work();
+            dynamic m = managerConstructor.Invoke(new object[] { "Petya" });
+            m.work();
+            dynamic p = programmerConstructor.Invoke(new object[] { "Denis" });
+            p.work();
+
+            Console.WriteLine(w.name);
+            Console.WriteLine(m.name);
+            Console.WriteLine(p.name);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Workers/Classes.cs
@@ -0,0 +1,31 @@
+﻿namespace Workers
+{
+    class Worker
+    {
+        public string name;
+        public Worker(string name)
+        {
+            this.name = name;
+        }
+        public virtual void work()
+        {
+            Console.WriteLine(name + " working");
+        }
+    }
+    class Manager : Worker
+    {
+        public Manager(string name) : base(name) { }
+        public override void work()
+        {
+            Console.WriteLine(name + " managing");
+        }
+    }
+    class Programmer : Worker
+    {
+        public Programmer(string name) : base(name) { }
+        public override void work()
+        {
+            Console.WriteLine(name + " programming");
+        }
+    }
+}
